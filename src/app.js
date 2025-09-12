935bd8812a6bbf5a7eed9005a4e4b719124e3715
Legionxoxo <chandelshiv45@gmail.com>
2025-09-13 00:04:43 +0530

    feat : logging and middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(helmet());
+app.use(cors());
+app.use(cookieParser());
+
+app.use(
+  morgan('combined', {
+    stream: {
+      write: message => {
+        logger.info(message.trim());
+      },
+    },
+  })
+);
 
 app.get('/', (req, res) => {
+  logger.info('Winston says Hola Amigo !!');
   res.status(200).send('Hello devops!!!!');
 });
 
